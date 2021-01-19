519807f26c36debe2e282f7215cc946d77b0fec2
c8610290927 <c8610290927@gmail.com>
2021-01-20 03:35:48 +0800

    Initial Commit

---
+++ 00757039_HW4_CRUD/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  00757039_HW4_CRUD
+//
+//  Created by User20 on 2021/1/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ 00757039_HW4_CRUD/_0757039_HW4_CRUDApp.swift
@@ -0,0 +1,17 @@
+//
+//  _0757039_HW4_CRUDApp.swift
+//  00757039_HW4_CRUD
+//
+//  Created by User20 on 2021/1/20.
+//
+
+import SwiftUI
+
+@main
+struct _0757039_HW4_CRUDApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
